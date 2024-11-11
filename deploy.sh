d2449e6e1bf3da88cd73bb2c373c24cae796348f
ndalton57 <nickdalton57@gmail.com>
2024-11-11 17:14:24 -0500

    Added deploy script

---
+++ deploy.sh
@@ -0,0 +1,17 @@
+rm -rf ~/.config/btop/*
+ln ./btop/* ~/.config/btop/
+
+rm -rf ~/.config/hypr/*
+ln ./hypr/* ~/.config/hypr/
+
+rm -rf ~/.config/kitty/*
+ln ./kitty/* ~/.config/kitty/
+
+rm -rf ~/.config/ranger/*
+ln ./ranger/* ~/.config/ranger/
+
+rm -rf ~/.config/waybar/*
+ln ./waybar/* ~/.config/waybar/
+
+rm -rf ~/.config/wofi/*
+ln ./wofi/* ~/.config/wofi/
